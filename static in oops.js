5286e09d009a94434933bbbeb4547c80bfc14a42
your name <ayushvk26102006@gmail.com>
2026-09-15 12:01:26 +0530

    static in oops

---
+++ static in oops.js
@@ -0,0 +1,11 @@
+class Student{
+    static Fname="Ayush";
+    static Lname="Sharma";
+     display(){
+        console.log(Student.Fname);
+        console.log(Student.Lname);
+    }
+}
+// Student.display();
+let  s1=new Student(); 
+s1.display();
